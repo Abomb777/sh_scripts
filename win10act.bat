b006f28fe94aa817ab99a589273de748159345de
Abomb <51196470+Abomb777@users.noreply.github.com>
2024-06-10 00:32:04 +0300

    Create win10act.bat

---
+++ win10act.bat
@@ -0,0 +1,32 @@
+slmgr.vbs /upk
+slmgr/ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+slmgr /skms zh.us.to
+slmgr/ato
+
+rem https://passper.imyfone.com/windows-10/windows-10-product-key-free/
+
+rem List of Windows 10 Product Keys for All Versions
+rem Windows 10 Professional Key	W269N-WFGWX-YVC9B-4J6C9-T83GX
+rem Windows 10 Professional N Product Key	MH37W-N47XK-V7XM9-C7227-GCQG9
+rem Windows 10 Enterprise Key	NPPR9-FWDCX-D2C8J-H872K-2YT43
+rem Windows 10 Enterprise N Key	DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4
+rem Windows 10 Education Key	NW6C2-QMPVW-D7KKK-3GKT6-VCFB2
+rem Windows 10 Home N	AKJUS-WY2CT-JWBJ2-T68TQ-YBH2V
+rem Windows 10 Enterprise 2015 LTSB N	JAHSU-QMPVW-D7KKK-3GKT6-VCFB2
+rem Windows 10 Pro for Workstations	AKSIU-WY2CT-JWBJ2-T68TQ-YBH2V
+rem Windows Pro N for Workstations	SJUY7-NFMTC-H88MJ-PFHPY-QJ4BJ
+rem Windows 10 Pro Education	AJUYS-8C467-V2W6J-TX4WX-WT2RQ
+rem Windows 10 Enterprise N	AJSU7-GRT3P-VKWWX-X7T3R-8B639
+rem Windows 10 Enterprise Key	ALSOI-MHBT6-FXBX8-QWJK7-DRR8H
+rem Windows 10 Enterprise S	8UY76-TNFGY-69QQF-B8YKP-D69TJ
+rem Windows 10 Enterprise G N	AJSUY-NPHTM-C97JM-9MPGT-3V66T
+rem Windows 10 Pro Education N	ALSOI-4C88C-JK8YV-HQ7T2-76DF9
+rem List of Windows 10 Activation Keys
+rem Windows Server 2016 Standard	WC2BQ-8NRM3-FDDYY-2BFGV-KHKQY
+rem Windows Server 2016 Essentials	JCKRF-N37P4-C2D82-9YXRT-4M63B
+rem Windows 10 Professional	W269N-WFGWX-YVC9B-4J6C9-T83GX
+rem Windows 10 Professional N	MH37W-N47XK-V7XM9-C7227-GCQG9
+rem Windows 10 Enterprise	NPPR9-FWDCX-D2C8J-H872K-2YT43
+rem Windows 10 Enterprise N	DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4
+rem Windows 10 Education	NW6C2-QMPVW-D7KKK-3GKT6-VCFB2
+rem Windows 10 Education N	2WH4N-8QGBV-H22JP-CT43Q-MDWWJ
