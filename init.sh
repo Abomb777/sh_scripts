8bcf868e6f3d428e0e4263d98a449e3409de64f8
Abomb <51196470+Abomb777@users.noreply.github.com>
2024-07-06 00:36:23 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,12 +2,12 @@
 
 #awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else print ($2+$4-u1) * 100 / (t-t1) "%"; }' <(grep 'cpu ' /proc/stat) <(sleep 1;grep 'cpu ' /proc/stat)
 USAGE=$(cat <(grep 'cpu ' /proc/stat) <(sleep 1 && grep 'cpu ' /proc/stat) | awk -v RS="" '{printf "%.2f", ($13-$2+$15-$4)*100/($13-$2+$15-$4+$16-$5)}')
-echo " $USAGE %"
+echo "CPU load: $USAGE %"
 CORES="$(nproc --all)"
-echo " $CORES"
+echo "Total cores: $CORES"
 #CORESINUSE=$(awk 'BEGIN { printf "%.2f", ($CORES/100) }')
 CORESINUSE=$(awk "BEGIN { printf \"%.0f\", ($CORES / 100 * $USAGE) }")
-echo " $CORESINUSE"
+echo "Cores in use: $CORESINUSE"
 #MAXTHREADS="$($CORES - $CORESINUSE)"
 MAXTHREADS=$(awk "BEGIN { printf \"%.0f\", ($CORES - $CORESINUSE) }")
-echo " $MAXTHREADS"
+echo "Can use max cores: $MAXTHREADS"
