00460b507251d686b787e4343ff30663c1f080f3
Abomb <51196470+Abomb777@users.noreply.github.com>
2024-07-05 20:12:12 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else print ($2+$4-u1) * 100 / (t-t1) "%"; }' <(grep 'cpu ' /proc/stat) <(sleep 1;grep 'cpu ' /proc/stat)
-USAGE=$(cat <(grep 'cpu ' /proc/stat) <(sleep 1 && grep 'cpu ' /proc/stat) | awk -v RS="" '{printf "%.2f%\n", ($13-$2+$15-$4)*100/($13-$2+$15-$4+$16-$5)}')
-echo " $USAGE"
+USAGE = $(cat <(grep 'cpu ' /proc/stat) <(sleep 1 && grep 'cpu ' /proc/stat) | awk -v RS="" '{printf "%.2f%\n", ($13-$2+$15-$4)*100/($13-$2+$15-$4+$16-$5)}')
+echo $USAGE
 CORES = $(nproc --all)
 echo " $CORES"
